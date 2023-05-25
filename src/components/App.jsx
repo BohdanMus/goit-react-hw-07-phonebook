46dda6ba0a70ce4796836c5f2d5abd1cd410ef65
BohdanMus <bogdanmusienko@gmail.com>
2023-05-25 20:38:32 +0300

    add file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { GlobalStyle } from './GlobalStyle';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { getContacts, addContact, deleteContact } from 'redux/contactSlice.js';
+import { useDispatch, useSelector } from 'react-redux';
+import { getFilter, setFilter } from 'redux/filterSlice';
+
 export const App = () => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  const dispatch = useDispatch();
+
+  const doAddContact = newContact => {
+    if (
+      contacts.some(
+        contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+      )
+    ) {
+      alert(`${newContact.name} is already in contacts`);
+    } else {
+      dispatch(addContact(newContact));
+    }
+  };
+  const doDeleteContact = id => {
+    dispatch(deleteContact(id));
+  };
+  const onChangeInput = e => {
+    dispatch(setFilter(e.currentTarget.value));
+  };
+
+  const filterNew = () => {
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    return filteredContacts;
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <GlobalStyle />
+      <h1>Phonebook</h1>
+      <ContactForm onAdd={doAddContact} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} onChangeInput={onChangeInput} />
+      <ContactList contacts={filterNew()} onDelete={doDeleteContact} />
     </div>
   );
 };
